fcfd35b13e4a8d006fe3c6b2b7300e4cf48e120c
RifqiSubchan02 <rifqisubchan@gmail.com>
2023-06-21 06:16:37 +0700

    slicing menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,57 +6,126 @@ import Box from '@mui/material/Box'
 import BottomNavigation from '@mui/material/BottomNavigation'
 import BottomNavigationAction from '@mui/material/BottomNavigationAction'
 import Paper from '@mui/material/Paper'
-import AppBar from '@mui/material/AppBar'
-import Typography from '@mui/material/Typography'
-import IconButton from '@mui/material/IconButton'
-import Badge from '@mui/material/Badge'
-import Toolbar from '@mui/material/Toolbar'
-import MailIcon from '@mui/icons-material/Mail'
-import NotificationsIcon from '@mui/icons-material/Notifications'
+import Avatar from '@mui/material/Avatar'
 import HomeIcon from '@mui/icons-material/Home'
-import AccountCircleIcon from '@mui/icons-material/AccountCircle'
-import AdbIcon from '@mui/icons-material/Adb'
 import MenuIcon from '@mui/icons-material/Menu'
+import Header from '@/components/header'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import Divider from '@mui/material/Divider'
+import Collapse from '@mui/material/Collapse'
+import ListItemAvatar from '@mui/material/ListItemAvatar'
+import Button from '@mui/material/Button'
+
+import InboxIcon from '@mui/icons-material/Inbox'
+import DraftsIcon from '@mui/icons-material/Drafts'
+import ExpandLess from '@mui/icons-material/ExpandLess'
+import ExpandMore from '@mui/icons-material/ExpandMore'
+import StarBorder from '@mui/icons-material/StarBorder'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import LogoutIcon from '@mui/icons-material/Logout'
 
 export default function Home() {
   const [value, setValue] = useState(0)
+  const [open, setOpen] = useState(true)
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ pb: 6 }}>
       <CssBaseline />
-      <AppBar position="static">
-        <Toolbar>
-          <AdbIcon sx={{ mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: 'none', sm: 'flex' } }}
-          >
-            MY APP
-          </Typography>
-          <Box sx={{ flexGrow: 1 }} />
-          <Box>
-            <IconButton
-              size="large"
-              aria-label="show 4 new mails"
-              color="inherit"
-            >
-              <Badge badgeContent={4} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              aria-label="show 17 new notifications"
-              color="inherit"
-            >
-              <Badge badgeContent={17} color="error">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
+      {value === 0 ? (
+        <Header />
+      ) : (
+        <Box sx={{ p: 1 }}>
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemAvatar>
+                  <Avatar />
+                </ListItemAvatar>
+                <ListItemText
+                  primary="The Kamp"
+                  secondary="View your profile"
+                />
+                <ChevronRightIcon />
+              </ListItemButton>
+            </ListItem>
+          </List>
+          <Divider />
+          <List>
+            <ListItemButton>
+              <ListItemIcon>
+                <DraftsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+            <ListItemButton onClick={() => setOpen(!open)}>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+              {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                <ListItemButton sx={{ pl: 4 }}>
+                  <ListItemIcon>
+                    <StarBorder />
+                  </ListItemIcon>
+                  <ListItemText primary="Starred" />
+                </ListItemButton>
+              </List>
+            </Collapse>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+          </List>
+          <Divider />
+          <List>
+            <ListItemButton>
+              <ListItemIcon>
+                <LogoutIcon />
+              </ListItemIcon>
+              <ListItemText primary="Logout" />
+            </ListItemButton>
+          </List>
+        </Box>
+      )}
       <Paper
         sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
         elevation={3}
@@ -69,10 +138,6 @@ export default function Home() {
           }}
         >
           <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-          <BottomNavigationAction
-            label="Profile"
-            icon={<AccountCircleIcon />}
-          />
           <BottomNavigationAction label="Menu" icon={<MenuIcon />} />
         </BottomNavigation>
       </Paper>
---
+++ src/components/header.tsx
@@ -0,0 +1,48 @@
+import AppBar from '@mui/material/AppBar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import Badge from '@mui/material/Badge'
+import Toolbar from '@mui/material/Toolbar'
+import MailIcon from '@mui/icons-material/Mail'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import AdbIcon from '@mui/icons-material/Adb'
+import Box from '@mui/material/Box'
+
+export default function Header() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <AdbIcon sx={{ mr: 1 }} />
+        <Typography
+          variant="h6"
+          noWrap
+          component="div"
+          // sx={{ display: { xs: 'none', sm: 'flex' } }}
+        >
+          MY APP
+        </Typography>
+        <Box sx={{ flexGrow: 1 }} />
+        <Box>
+          <IconButton
+            size="large"
+            aria-label="show 4 new mails"
+            color="inherit"
+          >
+            <Badge badgeContent={4} color="error">
+              <MailIcon />
+            </Badge>
+          </IconButton>
+          <IconButton
+            size="large"
+            aria-label="show 17 new notifications"
+            color="inherit"
+          >
+            <Badge badgeContent={17} color="error">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  )
+}
